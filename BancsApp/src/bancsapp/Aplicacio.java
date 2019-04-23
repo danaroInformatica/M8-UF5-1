7725b19c4e83534bb58514a3ecac08f84870c095
ossaoj <49898305+ossaoj@users.noreply.github.com>
2019-04-23 09:15:37 +0200

    Add files via upload

---
+++ BancsApp/src/bancsapp/Aplicacio.java
@@ -0,0 +1,37 @@
+package bancsapp;
+
+public class Aplicacio {
+
+    public static void main(String[] args) {
+
+        System.out.println("Introdueix el nom del compte bancari 1: ");
+        String nom = cLlegir.dato();
+        System.out.println(".................................................");
+        System.out.println("Introdueix el nombre secret del compte bancari 1: ");
+        int numero = cLlegir.datoInt();
+        System.out.println(".................................................");
+        CompteBancari compte = new CompteBancari(nom, numero);
+
+        System.out.println("Introdueix el nom del compte bancari 2: ");
+        nom = cLlegir.dato();
+        System.out.println(".................................................");
+        System.out.println("Introdueix el nombre seasdascret del compte bancari 2: ");
+        numero = cLlegir.datoInt();
+        System.out.println(".................................................");
+        CompteBancari compte2 = new CompteBancari(nom, numero);
+
+        System.out.println("Introdueix la quantitat que vols ingressar del compte bancari 2 "
+                + "al compte bancari 1  : ");
+        float quantitat = cLlegir.datoFloat();
+        boolean resultat = compte2.extreure(quantitat);
+        boolean resultat2 = compte.ingressar(quantitat);
+
+        if (resultat == false || resultat2 == false) {
+            System.out.println("No s'ha pogut realitzar l'ingrés!");
+        } else {
+            System.out.println("El saldo final del compte 1 és: " + compte.consultarSaldo());
+            System.out.println("El saldo final del compte 2 és: " + compte2.consultarSaldo());
+        }
+        System.out.println(".................................................");
+    }
+}
